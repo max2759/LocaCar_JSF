65754fab54d2d5f6c17aa1bdb34f04721ee05abe
Younes Arifi <younesarifi@hotmail.com>
2021-01-25 16:29:41 +0100

    US-14. Test insurances List

---
+++ src/main/java/be/atc/LocacarJSF/beans/InsurancesBean.java
@@ -0,0 +1,9 @@
+package be.atc.LocacarJSF.beans;
+
+import java.io.Serializable;
+
+public class InsurancesBean implements Serializable {
+    private static final long serialVersionUID = -8262263353009937764L;
+
+
+}
