f0b204d3e44dc5bba9d3eb9406fc9d4e07cd0d33
konzenon <60403827+konzenon@users.noreply.github.com>
2021-01-27 18:18:51 +0100

    US-24 squelette connexion HTML

--- src/main/java/be/atc/LocacarJSF/beans/UsersBean.java
+++ src/main/java/be/atc/LocacarJSF/beans/UsersBean.java
@@ -7,21 +7,49 @@ import org.apache.log4j.Logger;
 
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import java.io.Serializable;
 import java.util.List;
 
 @Named(value = "usersBean")
 @RequestScoped
-public class UsersBean {
+public class UsersBean implements Serializable {
     private static final long serialVersionUID = -8262263353009937764L;
     public static Logger log = Logger.getLogger(UsersBean.class);
 
-    private UsersEntity usersEntity;
-    private UsersServices usersServices;
+    private UsersEntity usersEntity = new UsersEntity();
+    private UsersServices usersServices = new UsersServicesImpl();
     private List<UsersEntity> usersEntities;
 
     public void init() {
-        usersServices = new UsersServicesImpl();
         usersEntities = usersServices.findAll();
+    }
+
+    public void connexion() {
+        log.info("je suis dans le connexion");
+    }
+
+    public UsersEntity getUsersEntity() {
+        return usersEntity;
+    }
+
+    public void setUsersEntity(UsersEntity usersEntity) {
+        this.usersEntity = usersEntity;
+    }
+
+    public UsersServices getUsersServices() {
+        return usersServices;
+    }
+
+    public void setUsersServices(UsersServices usersServices) {
+        this.usersServices = usersServices;
+    }
+
+    public List<UsersEntity> getUsersEntities() {
+        return usersEntities;
+    }
+
+    public void setUsersEntities(List<UsersEntity> usersEntities) {
+        this.usersEntities = usersEntities;
     }
 
     //verif mdp, ...
