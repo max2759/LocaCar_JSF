c4f606418f38e69256b7d9e369115b20044d1c66
YounesArifi <younesarifi@hotmail.com>
2021-02-10 17:24:52 +0100

    US-56 Add Validator leasing term. + Fix bug

---
+++ src/main/java/be/atc/LocacarJSF/validators/LeasingTimeValidator.java
@@ -0,0 +1,51 @@
+package be.atc.LocacarJSF.validators;
+
+import be.atc.LocacarJSF.beans.InsurancesBean;
+import org.apache.log4j.Logger;
+import utils.JsfUtils;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.util.Locale;
+
+/**
+ * @author Younes - Arifi
+ */
+@FacesValidator("leasingTimeValidator")
+public class LeasingTimeValidator implements Validator {
+
+    public static Logger log = Logger.getLogger(InsurancesBean.class);
+    Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    /**
+     * Validator for Price
+     *
+     * @param context
+     * @param component
+     * @param value
+     * @throws ValidatorException
+     */
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+        int i = (int) value;
+
+        if ((i != 36) && (i != 48) && (i != 60)) {
+            log.error("Leasing Time Error");
+            throw new ValidatorException(new FacesMessage(getMessageError()));
+        }
+
+    }
+
+    /**
+     * Return message for exception
+     *
+     * @return
+     */
+    private String getMessageError() {
+        return JsfUtils.returnMessage(locale, "fxs.basket.leasingTimeError");
+    }
+}
