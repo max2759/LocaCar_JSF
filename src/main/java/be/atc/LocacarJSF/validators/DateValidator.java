df7e460683eeb78357037935660563db40744157
max27 <max1>
2021-02-27 11:38:38 +0100

    US-71 Add jquery datepicker + date validator

---
+++ src/main/java/be/atc/LocacarJSF/validators/DateValidator.java
@@ -0,0 +1,41 @@
+package be.atc.LocacarJSF.validators;
+
+import utils.JsfUtils;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+@FacesValidator("dateValidator")
+public class DateValidator implements Validator {
+
+    Locale locale = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent uiComponent, Object value) throws ValidatorException {
+
+        LocalDateTime dateNow = LocalDateTime.now();
+
+        LocalDateTime valueDate = (LocalDateTime) value;
+
+        if (value != null) {
+            if (valueDate.isAfter(dateNow)) {
+                throw new ValidatorException(new FacesMessage(getMessageErrorDate()));
+            }
+        }
+    }
+
+    /**
+     * Return message for exception
+     *
+     * @return
+     */
+    private String getMessageErrorDate() {
+        return JsfUtils.returnMessage(locale, "fxs.dateError");
+    }
+}
