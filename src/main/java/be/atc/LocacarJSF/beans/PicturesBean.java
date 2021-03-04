53c754589b278fe18d8051f4547c947bae148b48
max27 <max1>
2021-03-04 16:34:13 +0100

    US-82 ads form + front & error update

--- src/main/java/be/atc/LocacarJSF/beans/PicturesBean.java
+++ src/main/java/be/atc/LocacarJSF/beans/PicturesBean.java
@@ -32,7 +32,7 @@ public class PicturesBean extends ExtendBean implements Serializable {
     private CarsPicturesServices carsPicturesServices = new CarsPicturesServicesImpl();
     private List<CarsPicturesEntity> carsPicturesEntityList;
 
-    private String folder = "A:\\Applications\\Drive\\Work\\IntelliJ\\uploads";
+    private String folder = "A:\\Applications\\Drive\\Work\\IntelliJ\\LocacarJSF\\src\\main\\webapp\\resources\\upload";
     private Part file;
 
 
